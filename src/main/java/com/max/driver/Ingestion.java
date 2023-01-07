3bd227d8c1c8c3839585963934cbcc83b64afc04
maxluong0204 <max.luong@geocomply.com>
2023-01-07 20:57:25 +0700

    2023-01-07 add code

---
+++ src/main/java/com/max/driver/Ingestion.java
@@ -0,0 +1,35 @@
+package com.max.driver;
+
+
+import com.max.service.SaleIngestionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import picocli.CommandLine;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@CommandLine.Command(name = "data-ingest")
+public final class Ingestion {
+
+    private final SaleIngestionService saleIngestionService;
+
+
+    Logger LOGGER = LoggerFactory.getLogger(Ingestion.class);
+
+    @CommandLine.Command(name = "master-sale")
+
+    public void ingestMasterSaleData(
+            @CommandLine.Option(names = "--file-name", required = true) String fileName
+    ) {
+        Dataset<Row> masterSale = saleIngestionService.loadCleanMasterSaleData(fileName);
+
+        masterSale.show();
+    }
+}
